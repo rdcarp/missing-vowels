dfdf8af75132bf0603bfd5367bec9fe9b1493b09
Richard Carp <richard.carp@clinithink.com>
2020-08-13 15:55:52 +0100

    Models for categories and answers.
    Fixes #1.

--- missingvowels/models.py
+++ missingvowels/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    A category of related answers (e.g. Web Browsers).
+    """
+    title = models.CharField(blank=False, max_length=64, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    """
+    A item within a Category (e.g. Edge, Firefox, Chrome, etc...)
+    """
+    class Meta:
+        unique_together = (('category', 'answer'))
+
+    answer = models.CharField(blank=False, max_length=128)
+    category = models.ForeignKey(
+        "Category", blank=False, on_delete=models.CASCADE)
+    render_format = models.CharField(blank=True, max_length=128)
+
+    def __str__(self):
+        return (
+            f"{self.category}: "
+            f"{self.render_format if self.render_format else ''.join([c.lower() for c in self.answer if c.lower() not in 'aeiou'])}"
+        )
