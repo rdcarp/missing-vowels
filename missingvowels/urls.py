c362487709144ae3435ad68fbf6d2b1b9bb4db74
Richard Carp <richard.carp@clinithink.com>
2020-11-09 20:23:47 +0000

    WIP: Came across these changes from a long time ago

--- missingvowels/urls.py
+++ missingvowels/urls.py
@@ -9,4 +9,5 @@ app_name = "missingvowels"
 urlpatterns = [
     path('', views.CategoryIndexView.as_view(), name='index'),
     path('<int:pk>', views.CategoryDetailView.as_view(), name='category'),
+    path('<int:pk>', views.CategoryDetailView.as_view(), name='answer'),
 ]
--- missingvowels/views.py
+++ missingvowels/views.py
@@ -15,3 +15,14 @@ class CategoryDetailView(generic.DetailView):
     template_name = "missingvowels/category/detail.html"
     model = Category
     context_object_name = "category"
+
+    @staticmethod
+    def provide_answers(request):
+        if request.method != "POST":
+            # TODO: return proper HTTP response.
+            return
+
+        # loop answers supplied
+        # TODO: confirm answers belong to current category.
+        # compare provided answer vs saved answer
+        # produce response object.
