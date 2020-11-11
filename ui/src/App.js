698dd4063f650c1a6d6d68c3168247eb1b5720e3
Richard Carp <richard.carp@clinithink.com>
2020-11-11 01:08:00 +0000

    Basic UI components.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactHtml from 'raw-html-react'
+import React from "react";
+
+
+const CorrectAnswer = () => (
+    <p>This is an answer</p>
+)
+
+const AnswerSubmit = () => (
+  <input type={"test"} />
+)
+
+const Clue = () => (
+  <p>HR S CL</p>
+)
+
+const Question = ({answered}) => (
+    <div>
+        <Clue />
+        { !answered && <AnswerSubmit /> }
+        { answered && <CorrectAnswer /> }
+    </div>
+)
+
+const Category = ({title, answered}) => (
+    <div>
+        <h4>{answered ? "True":"False"}</h4>
+        <Question answered={answered}/>
+    </div>
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h2>Page header.</h2>
       </header>
+      <Category title={"My missing vowels test."} answered={false}/>
     </div>
   );
 }
